4bb8f3690910837cc44d1c0a81783708b630ec72
Marko Polovina <markopolovina95@gmail.com>
2018-09-14 18:54:20 +0100

    izmjenev2

--- MultipleksServer/src/projektovanje/dbDAO/DBDAOProdavacKarata.java
+++ MultipleksServer/src/projektovanje/dbDAO/DBDAOProdavacKarata.java
@@ -24,6 +24,7 @@ public class DBDAOProdavacKarata implements IDBDAO {
 =======
     public Boolean azurirajBazu(IDTO list, Connection konekcijaNaBazu) {
 >>>>>>> 119457b79ea8ebc88d4b6e2705a6ba9d3cd5687a
+    public Boolean azurirajBazu(IDTO list, Connection konekcijaNaBazu) throws SQLException {
         return null;
     }
 
