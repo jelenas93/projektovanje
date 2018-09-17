d04a0ddfe5bd41f746d5e9a48e5d4a37a7b97a00
Zelimir <grahovac-zelimir@live.com>
2018-09-17 09:36:21 +0200

    DAO 1.7
    Napisana i testirana klasa PonudaDAO,Promjenjena metoda toString u klasi Ponuda

--- MultipleksServer/src/projektovanje/dbDAO/DBDAOPonuda.java
+++ MultipleksServer/src/projektovanje/dbDAO/DBDAOPonuda.java
@@ -1,35 +1,79 @@
 package projektovanje.dbDAO;
 
+import projektovanje.bin.film.Ponuda;
+import projektovanje.bin.oprema.Artikal;
+import projektovanje.bin.racun.Stavka;
 import projektovanje.dto.DTOPonuda;
+import projektovanje.dto.DTOStavka;
+import projektovanje.dto.DTOZaposleni;
 import projektovanje.dto.IDTO;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBDAOPonuda implements IDBDAO {
     @Override
-    public Boolean upisiUBazu(IDTO dtoInstanca, Connection konekcijaNaBazu) {
-        return null;
+    public Boolean upisiUBazu(IDTO dtoInstanca, Connection konekcijaNaBazu) throws SQLException {
+        DTOPonuda lokalniDTOPonuda = (DTOPonuda) dtoInstanca;
+        Ponuda lokalniPonuda = lokalniDTOPonuda.getPonuda();
+        PreparedStatement ps = konekcijaNaBazu.prepareStatement("insert into Ponuda values (default,?,?)");
+        ps.setDate(1,new Date(lokalniPonuda.getDatumPonude().getTime()));
+        ps.setInt(2,lokalniPonuda.getZaposleni().getIdZaposlenog());
+        ps.executeUpdate();
+        return true;
     }
 
     @Override
-    public List<DTOPonuda> citajIzBaze(Connection konekcijaNaBazu) {
-        return null;
+    public List<DTOPonuda> citajIzBaze(Connection konekcijaNaBazu) throws SQLException {
+        Statement s = konekcijaNaBazu.createStatement();
+        ResultSet rs = s.executeQuery("select * from Ponuda");
+        ArrayList<DTOPonuda> povratnaVrijednost = new ArrayList<>();
+        while (rs.next()){
+            Integer idPonude = rs.getInt(1);
+            java.util.Date datumPonude = new java.util.Date(rs.getDate(2).getTime());
+            Integer idZaposlenog = rs.getInt(3);
+            DTOZaposleni zaposleni = (DTOZaposleni) new DBDAOZaposleni().pretraziBazu(konekcijaNaBazu,String.valueOf(idZaposlenog));
+            Ponuda ponuda = new Ponuda(idPonude,null,datumPonude,zaposleni.getZaposleni());
+            povratnaVrijednost.add(new DTOPonuda(ponuda));
+        }
+        return povratnaVrijednost;
     }
 
     @Override
-    public Boolean azurirajBazu(IDTO list, Connection konekcijaNaBazu) throws SQLException {
-        return null;
+    public Boolean azurirajBazu(IDTO noviPonuda, Connection konekcijaNaBazu) throws SQLException {
+        DTOPonuda lokalniDTOPonuda = (DTOPonuda) noviPonuda;
+        Ponuda lokalniPonuda = lokalniDTOPonuda.getPonuda();
+        PreparedStatement ps = konekcijaNaBazu.prepareStatement("update Ponuda" +
+                "   set datumPonude = ?," +
+                "   idZaposlenog = ?" +
+                "   where idPonude = ?");
+        ps.setDate(1,new Date(lokalniPonuda.getDatumPonude().getTime()));
+        ps.setInt(2,lokalniPonuda.getZaposleni().getIdZaposlenog());
+        ps.setInt(3,lokalniPonuda.getIdPonude());
+        ps.executeUpdate();
+        return true;
     }
 
     @Override
-    public IDTO pretraziBazu(Connection konekcijaNaBazu, String parametarPretrage) {
-        return null;
+    public IDTO pretraziBazu(Connection konekcijaNaBazu, String parametarPretrage) throws SQLException {
+        DTOPonuda povratnaVrijednost = null;
+        int idPonude = Integer.valueOf(parametarPretrage);
+        PreparedStatement s = konekcijaNaBazu.prepareStatement("select * from Ponuda where idPonude = ?");
+        s.setInt(1,idPonude);
+        ResultSet rezultat = s.executeQuery();
+        if(rezultat.next()){
+            int id = rezultat.getInt(1);
+            java.util.Date datumPonude = new java.util.Date(rezultat.getDate(2).getTime());
+            int idZaposlenog = rezultat.getInt(3);
+            DTOZaposleni zaposleni = (DTOZaposleni) new DBDAOZaposleni().pretraziBazu(konekcijaNaBazu,String.valueOf(idZaposlenog));
+            povratnaVrijednost = new DTOPonuda(new Ponuda(id,null,datumPonude,zaposleni.getZaposleni()));
+        }
+        return povratnaVrijednost;
     }
 
     @Override
-    public List<DTOPonuda> ispisi(Connection konekcijaNaBazu) {
-        return null;
+    public List<DTOPonuda> ispisi(Connection konekcijaNaBazu) throws SQLException {
+        return citajIzBaze(konekcijaNaBazu);
     }
 }
--- MultipleksServer/src/projektovanje/test/MainTest.java
+++ MultipleksServer/src/projektovanje/test/MainTest.java
@@ -1,10 +1,13 @@
 package projektovanje.test;
 
+import projektovanje.bin.film.Ponuda;
 import projektovanje.bin.oprema.Artikal;
 import projektovanje.bin.racun.Stavka;
 import projektovanje.bin.transakcije.Racun;
 import projektovanje.bin.zaposleni.Zaposleni;
+import projektovanje.dbDAO.DBDAOPonuda;
 import projektovanje.dbDAO.DBDAORacun;
+import projektovanje.dto.DTOPonuda;
 import projektovanje.dto.DTORacun;
 import projektovanje.dto.DTOStavka;
 
@@ -19,34 +22,35 @@ public class MainTest {
         Connection c = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/multipleks", "root", "admin");
         Zaposleni z = new Zaposleni(1);
-        DBDAORacun dao = new DBDAORacun();
-        List<Stavka>stavke = new ArrayList<>();
-        Artikal a1 = new Artikal(1,"Smoki",5,4.3,"tip","123456",z);
-        Artikal a2 = new Artikal(2,"Cips",4,3.0,"jestivi","65432",z);
-        Stavka s1 = new Stavka(1,3,5.4,a1);
-        Stavka s2 = new Stavka(2,4,6.5,a2);
-        stavke.add(s1);
-        stavke.add(s2);
-        Racun racun = new Racun(1,stavke,new java.util.Date(),565.55,z);
+        DBDAOPonuda dao = new DBDAOPonuda();
+        //List<Stavka>stavke = new ArrayList<>();
+        //Artikal a1 = new Artikal(1,"Smoki",5,4.3,"tip","123456",z);
+        //Artikal a2 = new Artikal(2,"Cips",4,3.0,"jestivi","65432",z);
+        //Stavka s1 = new Stavka(1,3,5.4,a1);
+        //Stavka s2 = new Stavka(2,4,6.5,a2);
+        //stavke.add(s1);
+        //stavke.add(s2);
+        //Racun racun = new Racun(1,stavke,new java.util.Date(),565.55,z);
         System.out.println("pokusavama procitati sve:");
-        ArrayList<DTORacun> stavkaIzBaze = (ArrayList<DTORacun>) dao.citajIzBaze(c);
-        stavkaIzBaze.forEach(x->System.out.println(x.getRacun()));
+        ArrayList<DTOPonuda> stavkaIzBaze = (ArrayList<DTOPonuda>) dao.citajIzBaze(c);
+        stavkaIzBaze.forEach(x->System.out.println(x.getPonuda()));
 
+        Ponuda p = new Ponuda(1,null,new java.util.Date(),z);
         System.out.println("Pokusavam dodati u bazu");
-        dao.upisiUBazu(new DTORacun(racun),c);
+        dao.upisiUBazu(new DTOPonuda(p),c);
         System.out.println("Dodao");
         System.out.println("pokusavam opet procitati sve:");
-        stavkaIzBaze = (ArrayList<DTORacun>) dao.citajIzBaze(c);
-        stavkaIzBaze.forEach(x->System.out.println(x.getRacun()));
+        stavkaIzBaze = (ArrayList<DTOPonuda>) dao.citajIzBaze(c);
+        stavkaIzBaze.forEach(x->System.out.println(x.getPonuda()));
 
         System.out.println("Pokusavam update");
-        racun.setUkupndaCijena(546.0);
-        dao.azurirajBazu(new DTORacun(racun),c);
+        p.setDatumPonude(new java.util.Date(System.currentTimeMillis()+100000000));
+        dao.azurirajBazu(new DTOPonuda(p),c);
 
         System.out.println("Update gotov opet citam");
 
-        stavkaIzBaze = (ArrayList<DTORacun>) dao.citajIzBaze(c);
-        stavkaIzBaze.forEach(x->System.out.println(x.getRacun()));
+        stavkaIzBaze = (ArrayList<DTOPonuda>) dao.citajIzBaze(c);
+        stavkaIzBaze.forEach(x->System.out.println(x.getPonuda()));
 
         System.out.println("all done");
     }
